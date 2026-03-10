0324ce82dc1826fbab9f252f59d8d4003c9f684f
nheumsophoun <nheumsophoun@peakwealthbank.com>
2026-03-10 14:15:14 +0700

    enhance null for build

--- lib/src/extension/state_extension.dart
+++ lib/src/extension/state_extension.dart
@@ -16,10 +16,10 @@ extension ToValueNotifier<T> on T {
 }
 
 /// Vallue notifier builder function
-extension ValueNotifierAsWidgetBuilder<T> on ValueNotifier<T?> {
+extension ValueNotifierAsWidgetBuilder<T> on ValueNotifier<T> {
   /// Converts the ValueNotifier to a Widget that rebuilds when the value changes.
-  Widget builder({required Widget Function(T? value) build, Key? key}) {
-    return ValueListenableBuilder<T?>(
+  Widget builder({required Widget Function(T value) build, Key? key}) {
+    return ValueListenableBuilder<T>(
       valueListenable: this,
       builder: (context, value, child) {
         return build(value);
